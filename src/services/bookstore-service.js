bf5bf2b370bf1ac906a01bdeab773d231c38eb90
Anar Agaev <anar.n.agaev@gmail.com>
2019-12-25 16:04:49 +0300

    Initialize project

---
+++ src/services/bookstore-service.js
@@ -0,0 +1,7 @@
+// bookstore-service отведчает за получение данных о книгоа. В настоящем приложении этот скрипт работал бы с сервером
+
+export default class BookstoreService {
+  getBooks() {
+    return [];
+  }
+}
