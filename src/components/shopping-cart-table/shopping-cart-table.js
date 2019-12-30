411dfed73a030dd7d8ab1bf02f9996c369fe132e
Anar Agaev <anar.n.agaev@gmail.com>
2019-12-30 19:26:35 +0300

    reorganization code of reducer

--- src/components/shopping-cart-table/shopping-cart-table.js
+++ src/components/shopping-cart-table/shopping-cart-table.js
@@ -4,13 +4,13 @@ import { connect } from 'react-redux';
 import {
   bookAddedToCart,
   bookRemovedFromCart,
-  allBooksRemovedFromCart} from "../../actions";
+  allBooksRemovedFromCart } from '../../actions';
 
 import './shopping-cart-table.css';
 
 const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete }) => {
   const renderRow = (item, idx) => {
-    const {id, title, count, total} = item;
+    const { id, title, count, total } = item;
     return (
       <tr key={id}>
         <td>{idx + 1}</td>
@@ -34,7 +34,8 @@ const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete }) =
             <i className="fa fa-minus-circle" />
           </button>
         </td>
-      </tr>              );
+      </tr>
+    );
   };
 
   return (
@@ -42,17 +43,17 @@ const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete }) =
       <h2>Your Order</h2>
       <table className="table">
         <thead>
-          <tr>
-            <th>#</th>
-            <th>Item</th>
-            <th>Count</th>
-            <th>Price</th>
-            <th>Action</th>
-          </tr>
+        <tr>
+          <th>#</th>
+          <th>Item</th>
+          <th>Count</th>
+          <th>Price</th>
+          <th>Action</th>
+        </tr>
         </thead>
 
         <tbody>
-          { items.map(renderRow) }
+        { items.map(renderRow) }
         </tbody>
       </table>
 
@@ -63,7 +64,7 @@ const ShoppingCartTable = ({ items, total, onIncrease, onDecrease, onDelete }) =
   );
 };
 
-const mapStateToProps = ({ cartItems, orderTotal}) => {
+const mapStateToProps = ({ shoppingCart: { cartItems, orderTotal }}) => {
   return {
     items: cartItems,
     total: orderTotal
